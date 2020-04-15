4231f93245063bc911202ca4c31001aec16846cc
Irlan <irlan.freitas@hotmail.com>
2020-04-14 23:11:36 -0300

    Finalização de serviço criado com o Hapi.js

--- Modulo 10 - Criando servicos com hapi.js/src/routes/heroRoutes.js
+++ Modulo 10 - Criando servicos com hapi.js/src/routes/heroRoutes.js
@@ -1,20 +1,143 @@
-const Routes = require('./base/routes')
-
+const Routes = require("./base/routes");
+const Joi = require("joi");
+const Boom = require("boom")
+const failAction = (request, headers, erro) => {
+  throw erro;
+};
 class HeroRoutes extends Routes {
-    constructor(db) {
-        super()
-        this.db = db
-    }
+  constructor(db) {
+    super();
+    this.db = db;
+  }
 
-    list () {
-        return {
-            path: '/herois',
-            method: 'GET',
-            handler: (resquest, response) => {
-                return this.db.read()
-            }
+  list() {
+    return {
+      path: "/herois",
+      method: "GET",
+      config: {
+        validate: {
+          //payload == body
+          //header || head
+          //params -> na url :id
+          //query -> ?skip=10&limit=100
+          failAction,
+          query: {
+            skip: Joi.number().integer().default(0),
+            limit: Joi.number().integer().default(10),
+            nome: Joi.string().min(3).max(100),
+          },
+        },
+      },
+      handler: (request, headers) => {
+        try {
+          const { skip, limit, nome } = request.query;
+          const query = nome ? { nome: { $regex: `.*${nome}*.` } } : {};
+
+          return this.db.read(query, skip, limit);
+        } catch (error) {
+          console.log("DEU RUIM: ", error);
+          return Boom.internal();
         }
-    }
+      },
+    };
+  }
+
+  create() {
+    return {
+      path: "/herois",
+      method: "POST",
+      config: {
+        validate: {
+          failAction,
+          payload: {
+            nome: Joi.string().required().min(3).max(100),
+            poder: Joi.string().required().min(3).max(100),
+          },
+        },
+      },
+      handler: async (request, headers) => {
+        try {
+          const { payload } = request;
+
+          const result = await this.db.create(payload);
+
+          return { _id: result.id, message: "Heroi cadastrado com sucesso!" };
+        } catch (error) {
+          console.log("DEU RUIM: ", error);
+          return Boom.internal();
+        }
+      },
+    };
+  }
+
+  update() {
+    return {
+      path: "/herois/{id}",
+      method: "PATCH",
+      config: {
+        validate: {
+          failAction,
+          params: {
+            id: Joi.string().required(),
+          },
+          payload: {
+            nome: Joi.string().min(3).max(100),
+            poder: Joi.string().min(3).max(100),
+          },
+        },
+      },
+      handler: async (request, headers) => {
+        try {
+          const { id } = request.params;
+          const { payload } = request;
+
+          const dados = JSON.parse(JSON.stringify(payload));
+
+          const result = await this.db.update(id, dados);
+
+          if (result.nModified !== 1) {
+            return Boom.preconditionFailed("Id não encontrado")
+          }
+
+          return { message: "Heroi atualizado com sucesso" };
+        } catch (error) {
+          console.log("DEU RUIM: ", error);
+          return Boom.internal();
+        }
+      },
+    };
+  }
+
+  delete() {
+    return {
+      path: "/herois/{id}",
+      method: "DELETE",
+      config: {
+        validate: {
+          failAction,
+          params: {
+            id: Joi.string().required(),
+          },
+        },
+      },
+      handler: async (request) => {
+        try {
+          const { id } = request.params
+          const result = await this.db.delete(id)
+
+          if (result.deletedCount !== 1) {
+            return Boom.preconditionFailed("Id não encontrado")
+          }
+          
+          return { message: "Heroi deletado com sucesso" }
+
+        } catch (error) {
+          console.log("DEU RUIM: ", error);
+          return Boom.internal();
+        }
+      },
+    };
+  }
 }
 
-module.exports = HeroRoutes
+module.exports = HeroRoutes;
--- Modulo 10 - Criando servicos com hapi.js/src/tests/apiHerois.test.js
+++ Modulo 10 - Criando servicos com hapi.js/src/tests/apiHerois.test.js
@@ -3,12 +3,32 @@ const api = require("./../api");
 
 let app = {};
 
+const MOCK_DEFAULT_CADASTRAR = {
+  nome: "Static Shock",
+  poder: "To be black",
+};
+
+const MOCK_DEFAULT_ATUALIZAR = {
+  nome: "Hawkeye",
+  poder: "Anel mágico",
+};
+
+let MOCK_ID_UPLETE = null;
+
 describe.only("Suite de teste da API Herois", function () {
   this.beforeAll(async () => {
     app = await api;
+
+    const result = await app.inject({
+      method: "POST",
+      url: "/herois",
+      payload: MOCK_DEFAULT_ATUALIZAR,
+    });
+
+    MOCK_ID_UPLETE = JSON.parse(result.payload)._id;
   });
 
-  it("Listar /herois", async () => {
+  it("Listar GET /herois", async () => {
     const result = await app.inject({
       method: "GET",
       url: "/herois",
@@ -19,5 +39,124 @@ describe.only("Suite de teste da API Herois", function () {
     assert.deepEqual(statusCode, 200);
 
     assert.ok(Array.isArray(dados));
+  });
+
+  it("Listar GET /herois - Deve retornar somente 10 registros", async () => {
+    const LIMITE = 10;
+
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${LIMITE}`,
+    });
+
+    const dados = JSON.parse(result.payload);
+
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados.length === LIMITE);
+  });
+
+  it("Listar GET /herois - Deve invalidar o tipo limit", async () => {
+    const LIMITE = "AEE";
+
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${LIMITE}`,
+    });
+
+    const statusCode = result.statusCode;
+
+    assert.deepEqual(statusCode, 400);
+  });
+
+  it("Listar GET /herois - Deve filtrar um item", async () => {
+    const NOME = "Homem Aranha-1585785284666";
+
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=10&nome=${NOME}`,
+    });
+    const dados = JSON.parse(result.payload);
+
+    const statusCode = result.statusCode;
+
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados[0].nome === NOME);
+  });
+
+  it("Cadastrar POST - /herois", async () => {
+    const result = await app.inject({
+      method: "POST",
+      url: "/herois",
+      payload: MOCK_DEFAULT_CADASTRAR,
+    });
+
+    const { message, _id } = JSON.parse(result.payload);
+
+    assert.ok(result.statusCode === 200);
+    assert.notStrictEqual(_id, undefined);
+    assert.deepEqual(message, "Heroi cadastrado com sucesso!");
+  });
+
+  it("Atualizar PATCH - /herois/id", async () => {
+    const expected = { poder: "Ser foda" };
+
+    const result = await app.inject({
+      method: "PATCH",
+      url: `/herois/${MOCK_ID_UPLETE}`,
+      payload: expected,
+    });
+
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+
+    assert.ok(statusCode === 200);
+    assert.deepEqual(dados.message, "Heroi atualizado com sucesso");
+  });
+
+  it("Deletar DELETE - /herois/id ", async () => {
+    const result = await app.inject({
+      method: "DELETE",
+      url: `/herois/${MOCK_ID_UPLETE}`,
+    });
+
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+
+    assert.ok(statusCode === 200);
+    assert.deepEqual(dados.message, "Heroi deletado com sucesso");
+  });
+
+  it("Deletar DELETE - /herois/id - Não deve remover", async () => {
+    const result = await app.inject({
+      method: "DELETE",
+      url: `/herois/${MOCK_ID_UPLETE}`,
+    });
+
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+
+    assert.ok(statusCode === 412);
+    assert.deepEqual(dados.message, "Id não encontrado");
+  });
+
+  it("Atualizar PATCH - /heroi/id - Não deve atualizar com ID incorreto", async () => {
+
+    const result = await app.inject({
+      method: "PATCH",
+      url: `/herois/ID_INVALIDO`,
+      payload: { poder: "Ser foda" },
+    });
+
+    const statusCode = result.statusCode;
+    const dados = JSON.parse(result.payload);
+    const expected = {
+      statusCode: 500,
+      error: "Internal Server Error",
+      message: "An internal server error occurred",
+    };
+    
+    assert.ok(statusCode === 500);
+    assert.deepEqual(dados, expected);
   });
 });
